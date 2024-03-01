7415be09c2b2a3b69144e61254588fb6d2efae6c
MateusPMG <you@example.com>
2024-03-01 18:17:04 +0000

    webserv

---
+++ webserv.cpp
@@ -0,0 +1,12 @@
+#include "webserv.hpp"
+
+
+int main(int ac, char **av){
+
+    if (ac == 1 || ac == 2){
+        std::string configPath;
+        configPath = (ac == 1 ? "conf_files/default.conf" : av[1]);
+    }
+
+    return (0);
+}
