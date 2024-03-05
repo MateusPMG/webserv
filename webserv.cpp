02acb4a29d5ee342c2c1b2fc0c168d3728bed50a
MateusPMG <mpmgboss@gmail.com>
2024-03-05 14:30:11 +0000

    studying networking

--- webserv.cpp
+++ webserv.cpp
@@ -6,6 +6,7 @@ int main(int ac, char **av){
     if (ac == 1 || ac == 2){
         std::string configPath;
         configPath = (ac == 1 ? "conf_files/default.conf" : av[1]);
+        
     }
 
     return (0);
--- webserv.hpp
+++ webserv.hpp
@@ -1,3 +1,23 @@
 #pragma once
 
 #include <string>
+
+
+//NETWORKING
+
+//NOTES -> our adress family will be the AF_INET protocol since it is 
+//what is used in TCP/IP socket connections and we will be using stream sockets
+
+//offers socket methods for creation and usage of sockets
+//aka the doors through which our server receives and sends data
+#include <sys/socket.h>
+//for ip networking, we will be using the sockaddr_in struct to bind the sockets
+//aka to identify/name them by assigning a transport adress(a port number in IP networking)
+#include <netinet/in.h>
+
+class NetKing{
+    private:
+        
+    public:
+
+};
