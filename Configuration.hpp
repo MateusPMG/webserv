8fc1f55056454eb1a0f6a37a169e89de6c3353cd
MateusPMG <you@example.com>
2024-03-07 16:25:03 +0000

    i guess its going?

--- Configuration.hpp
+++ Configuration.hpp
@@ -1,2 +1,15 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
+class ConfigParser{
+	private:
+		std::string filepath;
+		int port;
+		std::string host;
+		std::string servername;
+		
+	public:
+
+};
