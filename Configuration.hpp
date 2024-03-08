d81ccb4ad032f7c11b6c3d36e88ec598f5c86036
MateusPMG <mpmgboss@gmail.com>
2024-03-08 14:28:52 +0000

    going

--- Configuration.hpp
+++ Configuration.hpp
@@ -2,14 +2,31 @@
 
 #include <iostream>
 #include <string>
-
+#include <map>
+#include <vector>
+// this class will parse the config file and store the server directives
+// no need for a destructor declaration since we dont need special cleanup
+// each member of the vector that contains the maps is a separate server config
+// the map will be divided into {section of config file, (directive name, directive value)}
 class ConfigParser{
 	private:
-		std::string filepath;
-		int port;
-		std::string host;
-		std::string servername;
-		
+		std::vector<std::map<std::string, std::map<std::string, std::string> > >configData;
+		void parseConfigFile(const std::string& filename);
 	public:
+		//constructor will call private parser
+		ConfigParser(const std::string& filename);
+		//getters to grant acess to the configs
+		int getNumServers()const;// 3 would mean 3 servers indexed 0,1,2
 
+		std::string getDirective(int serverIndex, const std::string& directiveName);
+		std::string getDirectory(int serverIndex, const std::string& route);
+		std::string getIndex(int serverIndex, const std::string& route);
+		std::string getCgi(int serverIndex, const std::string& route);
+		std::string getUploadDir(int serverIndex, const std::string& route);
+
+		std::map<std::string, std::string> getServerConfig(int serverIndex);
+		std::map<std::string, std::string> getErrorPages(int serverIndex);
+		std::map<std::string, std::string> getLimits(int serverIndex);
+
+		std::vector<std::string> getMethods(int serverIndex, const std::string& route);
 };
