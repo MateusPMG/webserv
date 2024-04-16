61cce0ffb2270c845fd3ee98e824e1d20cff9349
MateusPMG <you@example.com>
2024-04-16 14:42:00 +0100

    need help

---
+++ var/www/cgi-bin/simplecgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Print the content type header
+print("Content-type: text/html\n")
+
+# Print the HTML content
+print("<html>")
+print("<head><title>Simple CGI Example</title></head>")
+print("<body>")
+print("<h1>Simple CGI Example</h1>")
+print("<p>This is a simple CGI script written in Python.</p>")
+print("</body>")
+print("</html>")
